dcf75903736512ea71fa4d43fae825cf9cd41435
isa sand <isasandh@gmail.com>
2018-05-01 10:46:04 +0200

    fixed issues

--- WebSocketProject/src/java/model/MySQLClient.java
+++ WebSocketProject/src/java/model/MySQLClient.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -52,4 +52,101 @@ public class MySQLClient {
         return s;
     }
     
+    public class SensorTypes {
+        private int id;
+        private String sensorType;
+
+        public SensorTypes(int id,String sensorType) {         
+            this.id = id;
+            this.sensorType = sensorType;
+        }
+
+        /**
+         * @return the id
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id the id to set
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        /**
+         * @return the sensorType
+         */
+        public String getSensorType() {
+            return sensorType;
+        }
+
+        /**
+         * @param sensorType the sensorType to set
+         */
+        public void setSensorType(String sensorType) {
+            this.sensorType = sensorType;
+        }
+
+    }
+    public List<SensorTypes> getSensortypes() throws SQLException{
+        List<SensorTypes> typeList = new ArrayList<>();
+        
+        Statement stmt = con.createStatement(); 
+        ResultSet rs = stmt.executeQuery("SELECT * FROM SensorTypes");
+        
+        while(rs.next()){
+            int id = rs.getInt("idtypes");            
+            String type = rs.getString("type");
+            
+            typeList.add(new SensorTypes(id,type));
+        } 
+        return typeList;
+    }
+    
+    
+    
+    
+    public List<Sensor> getAllData() throws SQLException{
+//        private String id;
+//        private String type;
+//        private String data;
+//        private String time;
+        List<Sensor> sensorList = new ArrayList<>();
+        
+        Statement stmt = con.createStatement(); 
+        ResultSet rs = stmt.executeQuery("SELECT sensorIndex,sensorData,type, sensorTimestamp FROM Sensor,SensorTypes,Sensors\n" +
+"where idSensor = idSensors AND sensorType = idtypes;;");
+        
+        while(rs.next()){
+            String id = rs.getString("sensorIndex");
+            String data = rs.getString("sensorData");
+            String time = rs.getString("sensorTimestamp");
+            
+            String type = rs.getString("type");
+            
+            
+             
+            sensorList.add(new Sensor(data, id, time, type));
+        } 
+        return sensorList;
+    }
+    
+    public List<Sensor> getHistoricalData(int amount) throws SQLException{
+        List<Sensor> sensorList = new ArrayList<>();
+        Statement stmt = con.createStatement(); 
+        ResultSet rs = stmt.executeQuery("SELECT * FROM data");
+        
+        while(rs.next()){
+            String time = rs.getString("time");
+            
+            String type = rs.getString("type");
+            String data = rs.getString("data");
+            
+            String id = rs.getString("id"); 
+            sensorList.add(new Sensor(data, id, time, type));
+        } 
+        return sensorList;
+    }
 }
